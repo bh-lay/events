4de2de7c011d69bb37ce277fda2f5b8e21e773e2
bh_lay <bh_lay@126.com>
2016-09-02 18:54:25 +0800

    增加解除事件支持

--- events.js
+++ events.js
@@ -14,18 +14,43 @@
 	global.util.events = factory.events;
 	global.util.events.extend = factory.extend;
 })(window,function(exports){
+	function isNotEmptyString( input ){
+		return typeof( input ) === 'string' && input.length > 0
+	}
 	function isFunction( input ){
 		return typeof( input ) === 'function'
 	}
 	//监听自定义事件
-	function ON(eventName,callback){
+	function ON( eventName, callback ){
 		this._events = this._events || {};
 
-		if( isFunction( callback ) ){
+		if( isNotEmptyString( eventName ) && isFunction( callback ) ){
 			//事件堆无该事件，创建一个事件堆
 			this._events[eventName] = this._events[eventName] || [];
 			// 追加至事件列表
 			this._events[eventName].push( callback );
+		}
+		//提供链式调用的支持
+		return this;
+	}
+	//解除自定义事件监听
+	function UN( eventName, callback ){
+		var eventList = ( this._events || {} )[eventName];
+		//事件堆无该事件队列，或未传入事件名结束运行
+		if( !eventList || !isNotEmptyString( eventName ) ){
+			return
+		}
+		// 若未传入回调参数，则直接置空事件队列
+		if( !isFunction( callback ) ){
+			eventList = [];
+		}else{
+			// 逆序遍历事件队列
+			for( var i = eventList.length-1; i!=-1; i-- ){
+				// 回调相同，移除当前项
+				if( eventList[i] == callback ){
+					eventList.splice(i,1);
+				}
+			}
 		}
 		//提供链式调用的支持
 		return this;
@@ -47,6 +72,7 @@
 	function EXTEND( global ){
 		global._events = {};
 		global.on = ON;
+		global.un = UN;
 		global.emit = EMIT;
 	}
 	function EVENTS( global ){
@@ -55,6 +81,7 @@
 	}
 	EVENTS.prototype = {
 		on : ON,
+		un: UN,
 		emit : EMIT
 	};
 	
