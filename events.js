9779f009501517ff28bef3eaff1e4fe775ff0aaf
bh_lay <bh_lay@126.com>
2016-09-02 18:17:42 +0800

    增加参数检测，调整注释

--- events.js
+++ events.js
@@ -14,19 +14,25 @@
 	global.util.events = factory.events;
 	global.util.events.extend = factory.extend;
 })(window,function(exports){
-
-	//处理自定义事件
+	function isFunction( input ){
+		return typeof( input ) === 'function'
+	}
+	//监听自定义事件
 	function ON(eventName,callback){
 		this._events = this._events || {};
-		//事件堆无该事件，创建一个事件堆
-		if(!this._events[eventName]){
-			this._events[eventName] = [];
+
+		if( isFunction( callback ) ){
+			//事件堆无该事件，创建一个事件堆
+			this._events[eventName] = this._events[eventName] || [];
+			// 追加至事件列表
+			this._events[eventName].push( callback );
 		}
-		this._events[eventName].push(callback);
 		//提供链式调用的支持
 		return this;
 	}
+	// 主动触发自定义事件
 	function EMIT( eventName ){
+		// 获取除了事件名之外的参数
 		var args = Array.prototype.slice.call( arguments, 1, arguments.length );
 		this._events = this._events || {};
 		//事件堆无该事件，结束运行
@@ -34,7 +40,7 @@
 			return
 		}
 		for(var i=0,total=this._events[eventName].length;i<total;i++){
-			this._events[eventName][i].apply(this._event_global || this,args);
+			this._events[eventName][i].apply( this._event_global || this, args );
 		}
 	}
 	//继承
